f6864d5407b4596fdd39bba9709f7d3801b59a19
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2025-09-15 15:58:25 +0200

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import { Devtool } from '@flowerforce/devtool'
 import { createRoot } from 'react-dom/client'
-
 import { App } from './App'
-import { StrictMode } from 'react'
 import { AbacProvider, FlowerProvider } from '@flowerforce/flower-react'
 
-// Devtool({
-//   port: 8770,
-//   host: 'localhost',
-//   sessionId: '',
-// });
 
 createRoot(document.getElementById('root')!).render(
   <FlowerProvider>
